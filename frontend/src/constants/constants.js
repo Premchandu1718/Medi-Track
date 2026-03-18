28e4836aaa5d58f9e223d665514423d59b5f1578
Premchandu <www.ramajipremchandu@gmail.com>
2026-03-18 16:33:55 +0530

    fix base url for production

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -1,3 +1,4 @@
-export const BASE_URL = process.env.NODE_ENV === "development"
-  ? "http://localhost:5000/api"
-  : "/api";
+export const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000/api"
+    : "https://medi-track-backend-p4gm.onrender.com/api";
